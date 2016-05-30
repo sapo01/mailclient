472cb1c7910d947b970cf9446fc4ec05bf810be7
TAAPOSA4 <TAAPOSA4@U265856.corproot.net>
2016-05-30 11:36:42 +0200

    First Code

---
+++ src/MailPreferences.java
@@ -0,0 +1,48 @@
+import java.util.prefs.Preferences;
+
+public class MailPreferences {
+
+	private static final String userID = "UserID";
+	private static final String userAddress = "UserAddress";
+	private static final String passwd = "Passwd";
+
+	private Preferences prefs;
+	
+	public MailPreferences() {
+		// This will define a node in which the preferences can be stored
+		prefs = Preferences.userRoot().node(this.getClass().getName());
+	}
+
+	public String getUserName() {
+		return prefs.get(userID, "UserID");
+	}
+	
+	public String getUserAdress() {
+		return prefs.get(userAddress, "EMailAddress");
+	}
+	
+	public String getPassword() {
+		return prefs.get(passwd, "Passwort");
+	}
+
+	public void setUser(String userAdress, String password) {
+		setUser(userAdress, userAdress, password);
+	}
+
+	public void setUser(String userName, String userAddr, String password) {
+		prefs.put(userID, userName);
+		prefs.put(userAddress, userAddr);
+		prefs.put(passwd, password);
+	}
+
+	public static void main(String[] args) {
+		MailPreferences test = new MailPreferences();
+		System.out.println(test.getUserName());
+		System.out.println(test.getUserAdress());
+		System.out.println(test.getPassword());
+		test.setUser(
+			  	"urs-martin.kuenzi@gmail.com", 
+				"urs-martin.kuenzi@gmail.com", 
+				"********");
+	}
+}
---
+++ src/ReceiveMail.java
@@ -0,0 +1,125 @@
+import java.io.*;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+/**
+ * A simple program for receiving mail messages from gmail using POP3S.
+ *
+ * @version 02-APR-2015
+ */
+public class ReceiveMail {
+	/**
+	 * Receives email via POP3.
+	 *
+	 * @param args
+	 *            Not used
+	 */
+	public static void main(String[] args) {
+		// The IP address of the POP3 server
+		String host = "pop.gmail.com";
+
+		// Username and password
+		MailPreferences prefs = new MailPreferences();
+		String user = prefs.getUserName();
+		String password = prefs.getPassword();
+
+		// Get system properties
+		Properties properties = System.getProperties();
+
+		// Request POP3S
+		properties.put("mail.store.protocol", "pop3s");
+
+		// Get the default Session object
+		Session session = Session.getDefaultInstance(properties);
+
+		try {
+			// Get a store for the POP3S protocol
+			Store store = session.getStore();
+
+			// Connect to the current host using the specified username and
+			// password
+			store.connect(host, user, password);
+
+			// Create a Folder object corresponding to the given name
+			Folder folder = store.getFolder("inbox");
+
+			// Open the Folder
+			folder.open(Folder.READ_WRITE);
+
+			// Get the messages from the server
+			Message[] messages = folder.getMessages();
+
+			// Display message
+			for (int i = 0; i < messages.length; i++) {
+				Message msg = messages[i];
+
+				String from = InternetAddress.toString(msg.getFrom());
+				if (from != null) {
+					System.out.println("From: " + from);
+				}
+
+				String to = InternetAddress.toString(msg.getRecipients(Message.RecipientType.TO));
+				if (to != null) {
+					System.out.println("To: " + to);
+				}
+
+				String subject = msg.getSubject();
+				if (subject != null) {
+					System.out.println("Subject: " + subject);
+				}
+
+				Date sent = msg.getSentDate();
+				if (sent != null) {
+					System.out.println("Sent: " + sent);
+				}
+
+				// Empty line to separate header from body
+				System.out.println();
+
+				// This could lead to troubles if anything but text was sent
+				printMessage(msg);
+
+				System.out.println("--------------------------------------");
+				System.out.println();
+
+				/*
+				 * In der endgueltigen Version sollen die Mails geloescht werden
+				 */
+
+				// Mark this message for deletion when the session is closed
+				// msg.setFlag( Flags.Flag.DELETED, true ) ;
+			}
+
+			folder.close(true);
+			store.close();
+		} catch (MessagingException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void printMessage(Message message) throws IOException, MessagingException {
+		Part messagePart = message;
+		Object content = messagePart.getContent();
+		// -- or its first body part if it is a multipart message --
+		if (content instanceof Multipart) {
+			messagePart = ((Multipart) content).getBodyPart(0);
+			System.out.println("[ Multipart Message with " + ((Multipart) content).getCount() + " parts]");
+		}
+		// -- Get the content type --
+		String contentType = messagePart.getContentType();
+		// -- If the content is plain text, we can print it --
+		System.out.println("CONTENT:" + contentType);
+		if (contentType.startsWith("text/plain") || contentType.startsWith("text/html")) {
+			InputStream is = messagePart.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			String thisLine = reader.readLine();
+			while (thisLine != null) {
+				System.out.println(thisLine);
+				thisLine = reader.readLine();
+			}
+		}
+		System.out.println("-----------------------------");
+	}
+
+}
